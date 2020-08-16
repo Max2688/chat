a5d4ebd32d59f341c265913c816009b94fdd304f
max2688 <jumper2388@gmail.com>
2020-08-16 18:49:24 +0300

    chat ready

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="UTF-8">
     <title>Chat</title>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link href="css/style.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-<form action="">
-    <div id="chat-result" class="chat-result">
 
+<form id="chat" action="">
+    <div class="chat-result" id="chat-result">
+        <input type="text" name="chat-user" id="chat-user" placeholder="Name">
+        <input type="text" name="chat-message" id="chat-message"  placeholder="Message">
+        <input type="submit" value="Send" >
     </div>
 </form>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
+
 function message(text) {
     jQuery('#chat-result').append(text);
 }
 
-jQuery(document).ready( function () {
- var socket = new WebSocket("ws://localhost:8080/chat/server.php");
-    socket.onopen = function () {
-        message('<p>Connection created</p>');
+jQuery(document).ready(function($) {
+
+    var socket = new WebSocket("ws://localhost:8090/chat/server.php");
+
+    socket.onopen = function() {
+        message("<div>Connection created</div>");
     };
 
-    socket.onerror = function (error) {
-        message("<p>Error connection: " + (error.message ? error.message : '')+'</p>')
+    socket.onerror = function(error) {
+        message("<div>Error connection" + (error.message ? error.message : "") + "</div>");
     };
 
-    socket.onclose = function () {
-        message('<p>Connection close</p>')
+    socket.onclose = function() {
+        message("<div>Connection close</div>");
     };
 
-    socket.onmessage = function (event) {
-      var data = JSON.parse(event.data);
-      message("<p>"+ data.type + "-" + data.message +"</p>")
-    }
+    socket.onmessage = function(event) {
+        var data = JSON.parse(event.data);
+        message("<div>" + data.type + " - " + data.message + "</div>");
+    };
+
+    $("#chat").on('submit',function() {
+
+        var message = {
+            chat_message:$("#chat-message").val(),
+            chat_user:$("#chat-user").val(),
+        };
+
+        $("#chat-user").attr("type","hidden");
+
+        socket.send(JSON.stringify(message));
+
+        return false;
+    });
 });
